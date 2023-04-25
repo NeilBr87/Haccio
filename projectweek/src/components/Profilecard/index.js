8b9d66f794a24f36605823d68cc8714f6826be4f
NeilBrooks <neil.brooks87@gmail.com>
2023-04-25 12:29:47 +0100

    Started setting up array

---
+++ projectweek/src/components/Profilecard/index.js
@@ -0,0 +1,44 @@
+
+import React from "react";
+import Comment from "../Comment";
+import { useState } from "react";
+import commentForm from "../CommentForm";
+import "./commentList.css";
+
+
+
+function PeopleList(props) {
+  const peopleArray = [
+    {
+      id: "abc1",
+      Name: "Adam Adams",
+      imageURL: "https://www.realmenrealstyle.com/wp-content/uploads/2021/07/5-10.jpg"
+      HTMLvalue: 90,
+      CSSvalue: 80,
+      JSvalue: 70,
+      Jestvalue: 60,
+      Reactvalue: 60,
+      Playwrightvalue: 50,
+      githubprofile: "http://github.com/adamadams",
+      Linkedin: "http://linkedin.com/adamadams",
+      Slack: "http://slack.com/adamadams",
+      Email: "AdamAdams@gmail.com"
+    },
+    {
+      id: "abc2",
+      author: "React.JS Shillbot 54634",
+      content: "You're just salty cause you're not getting it straight away.",
+    },
+    {
+      id: "abc3",
+      author: "Mister_Node",
+      content: "Both of you shut up, we all know I'm the trickiest.",
+    }
+
+  ]
+
+  return {
+    <div>
+
+    </div>
+  }
