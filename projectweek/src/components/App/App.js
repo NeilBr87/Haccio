51d4de121599321e43ee8dcf97a8b6b9cb00ec23
NeilBrooks <neil.brooks87@gmail.com>
2023-04-25 12:20:33 +0100

    Components tree

---
+++ projectweek/src/components/App/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import Dashboard from "../Dashboard/Dashboard";
+
+
+function App() {
+  return <div>
+    <Dashboard />
+    <Navbar />
+    <Peoplelist />
+    <Profilecard />
+  </div>;
+}
+
+export default App;
