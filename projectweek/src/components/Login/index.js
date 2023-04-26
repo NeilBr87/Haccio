90dcf68191bdadef007a59b0b34cb492ef00b08b
tomwebb2022 <tommyttom10@gmail.com>
2023-04-26 14:25:37 +0100

    trying to change icons

--- projectweek/src/components/Login/index.js
+++ projectweek/src/components/Login/index.js
@@ -1,38 +1,56 @@
 import React from 'react'
 import './style.css'
-import { useState } from 'react'
+import { useState} from 'react'
 
-const loginForm = () => {
+const LoginForm = () => {
 
-  const [popupStyle,showPopup]= useState(hide)
+  // useEffect(() => {
+  //   function start() {
+  //       gapi.client.init({
+  //           clientId: "79474543031-tmjo35916ufn421ej3u1i2ljao2apr4s.apps.googleusercontent.com",
+  //           scope: ""
+  //       })
+  //   }
+  //   gapi.load('client: auth2', start)
+  // }, []) // added empty dependency array to only call useEffect once
+
+  const [popupStyle, showPopup] = useState("hide")
 
   const popup = () => {
     showPopup("login-popup")
     setTimeout(() => showPopup("hide"), 3000)
-
-  
-
   }
 
+  // const onSuccess = e => {
+  //   alert("User signed in")
+  //   console.log(e)
+  // }
 
+  // const onFailure = e => {
+  //   alert("User sign in Failed")
+  //   console.log(e)
+  // }
+  
   return (
     <div className="cover">
-    <h1>Login</h1>
-    <input type ="text" placeholder="username"/>
-    <input type ="password" placeholder="password"/>
-   
+      <h1>Login</h1>
+      <input type="text" placeholder="username"/>
+      <input type="password" placeholder="password"/>
 
-    <div className = "login-btn">Login</div>
-    
-    <p className='text'> Or login using</p>
+      <button className="login-btn" onClick={popup}>Login</button>
 
-    <div className="alt-login"> 
-      <div className = "Google"></div>
-      <div className = "GitHub"></div>
-    </div> 
+      <p className='text'> Or login using</p>
+
+      <div className="alt-login">
+        <div className="Google"></div>
+        <div className="GitHub"></div>
+      </div>
+      <div className={popupStyle}>
+        <h3>Login Failed</h3>
+        <p>Username or password incorrect</p>
+      </div>
     </div>
   )
 }
 
-
-export default loginForm
+export default LoginForm
